adb63047837feff7dcc2c7ec3926e8104339f85c
unknown <Aleksandra Marushchak>
2019-12-03 13:20:14 +0200

    first commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ add.addEventListener("click", function(){
     // var node = document.getElementsByClassName("row1")[0];
 
     // document.getElementsByClassName("wrap")[0].appendChild(node);
-    var node = '<div class="row1"><form class="form-inline"> <div class="form-group mb-2"><label for="staticEmail2" class="sr-only">Email</label><input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="email@example.com"></div><div class="form-group mx-sm-3 mb-2"> <label for="inputPassword2" class="sr-only">Password</label> <input type="password" class="form-control" id="inputPassword2" placeholder="Password"> </div> <div class="form-group"> <label for="exampleFormControlFile1"></label> <input type="file" class="form-control-file" id="exampleFormControlFile1"> </div></form></div>';
+    var node = '<div class="row1"><form class="form-inline"><div class="form-group mb-2"><label for="staticEmail2" class="sr-only">Email</label><input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="email@example.com"></div><div class="form-group mx-sm-3 mb-2"><label for="inputPassword2" class="sr-only">Password</label><input type="password" class="form-control" id="inputPassword2" placeholder="Password"></div><div class="form-group"><label for="exampleFormControlFile1"></label><input type="file" class="form-control-file" id="exampleFormControlFile1"></div></form></div>';
     document.getElementsByClassName("wrap")[0].innerHTML =  document.getElementsByClassName("wrap")[0].innerHTML + node;
 })
 
